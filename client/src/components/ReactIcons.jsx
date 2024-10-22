45f8428a67ef0846f73093965acf89c7d50f40d8
victory oseiwe <thisisvictoryoseiwe@gmail.com>
2024-10-22 14:46:46 +0100

    Navbar and navigation working navbar is working for both desktop and mobile

---
+++ client/src/components/ReactIcons.jsx
@@ -0,0 +1,22 @@
+import { FiMenu } from "react-icons/fi";
+import { MdOutlineCancel } from "react-icons/md";
+import { FaYoutube } from "react-icons/fa";
+import { IoLogoFacebook } from "react-icons/io5";
+import { FaInstagram } from "react-icons/fa";
+import { SiTiktok } from "react-icons/si";
+import { AiOutlineQuestionCircle } from "react-icons/ai";
+import { GrInstallOption } from "react-icons/gr";
+import { IoMdMoon } from "react-icons/io";
+import { IoSunny } from "react-icons/io5";
+
+// Exporting icons for use in other components
+export const Menu = FiMenu;
+export const Moon = IoMdMoon;
+export const Cancel = MdOutlineCancel;
+export const Youtube = FaYoutube;
+export const Facebook = IoLogoFacebook;
+export const Instagram = FaInstagram;
+export const Tiktok = SiTiktok;
+export const Question = AiOutlineQuestionCircle;
+export const Install = GrInstallOption;
+export const Sun = IoSunny;
--- client/src/components/head/Header.jsx
+++ client/src/components/head/Header.jsx
@@ -1,42 +1,94 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import moon from "../../assets/moon.svg";
-import { IoMdMenu } from "react-icons/io";
-import Icon from "../icon/Icon";
+import React, { useState } from "react";
+import { NavLink, Link } from "react-router-dom";
 import "./Header.css";
-import Facebook from "../../pages/facebook/Facebook";
+import {
+  Cancel,
+  Facebook,
+  Instagram,
+  Install,
+  Menu,
+  Moon,
+  Question,
+  Sun,
+  Tiktok,
+  Youtube,
+} from "../ReactIcons";
 
 export default function Header() {
+  const [sidebar, setSideBar] = useState(false);
+
+  function showSideBar() {
+    setSideBar(!sidebar);
+  }
   return (
     <>
       <div className="nav-container">
-        <div className="gradient-text">
+        <Link className="gradient-text" to="/">
           <span className="logo">socialdown</span>
-        </div>
+        </Link>
         <div className="nav-link">
-          <Link to="/youtube">
+          <NavLink className="link" to="/youtube">
             {/* <Icon icon={yt} /> */}
             <span>Youtube</span>
-          </Link>
-          <Link to="/facebook">
+          </NavLink>
+          <NavLink className="link" to="/facebook">
             <span>Facebook</span>
-          </Link>
-          <Link to="/instagram">
+          </NavLink>
+          <NavLink className="link" to="/instagram">
             <span>Instagram</span>
-          </Link>
-          <Link to="/tiktok">
+          </NavLink>
+          <NavLink className="link" to="/tiktok">
             <span>Tiktok</span>
-          </Link>
-          <Link to="/howto">
+          </NavLink>
+          <NavLink className="link" to="/howto">
             <span>How to</span>
-          </Link>
-          <Link to="/install">
+          </NavLink>
+          <NavLink className="link" to="/install">
             <span>Install</span>
-          </Link>
-          <Icon icon={moon} />
-          <IoMdMenu className="icon menu" />
+          </NavLink>
+          <Moon size={20} />
+        </div>
+        <div className="naviconcontainer">
+          <div className="iconwrapper">
+            <Moon size={20} />
+          </div>
+          <div className="iconwrapper">
+            <Menu size={20} onClick={showSideBar} className="menu" />
+          </div>
         </div>
       </div>
+      <div className={`sidebar ${sidebar ? "show" : ""}`}>
+        <div className="close iconwrapper">
+          <Cancel size={20} onClick={showSideBar} />
+        </div>
+        <div className="navigation">
+          <NavLink className="linkmobile" to="/youtube">
+            <Youtube />
+            <span>Youtube</span>
+          </NavLink>
+          <NavLink className="linkmobile" to="/facebook">
+            <Facebook />
+            <span>Facebook</span>
+          </NavLink>
+          <NavLink className="linkmobile" to="/instagram">
+            <Instagram />
+            <span>Instagram</span>
+          </NavLink>
+          <NavLink className="linkmobile" to="/tiktok">
+            <Tiktok />
+            <span>Tiktok</span>
+          </NavLink>
+          <NavLink className="linkmobile" to="/howto">
+            <Question />
+            <span>How to</span>
+          </NavLink>
+          <NavLink className="linkmobile" to="/install">
+            <Install />
+            <span>Install</span>
+          </NavLink>
+        </div>
+      </div>
+      {sidebar && <div className="overlay" onClick={showSideBar}></div>}
     </>
   );
 }
